46848492683b72b2289e698605f97a5edd7dd061
emiralles <emiralles_yataco@hotmail.com>
2022-03-29 01:52:15 +0200

    mod app
    modifiy app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Carousel from "./components/carousel/Carousel";
 import NavBar from "./components/navbar/NavBar";
-
 import CardGroup from "./components/cardgroup/CardGroup";
 import FooterComponent from "./components/footer/Footer";
 import Esdeveniments from "./components/esdeveniments/Esdeveniments";
+import { Routes, Route} from "react-router-dom";
+import Quisom from "./components/afa/quisom";
 
 function App() {
   return (
@@ -15,8 +16,11 @@ function App() {
       <main>
         <Carousel/>
         <div className='container marketing'>
-          <CardGroup/>
-          <Esdeveniments/>
+          <Routes>
+            <Route path='/' element={<CardGroup/>} />
+            <Route path='/esdeveniments' element={<Esdeveniments/>} />
+            <Route path='/quisom' element={<Quisom/>} />
+          </Routes>
         </div>
       </main>
       <footer className='App-Footer d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top'>
