9f308f58e363b9b2779903abd2268c7f2adcd375
emiralles <emiralles_yataco@hotmail.com>
2022-03-28 17:47:27 +0200

    card
    cardgroup

--- src/components/cardgroup/CardGroup.js
+++ src/components/cardgroup/CardGroup.js
@@ -1,120 +1,65 @@
-import image1 from "./images/img1.jpg";
-import "./cardGroup.css";
+import imag1 from "../carousel/images/img2.jpg";
 
 function CardGroup() {
     return (
         <>
             <div className="row">
-                <div className="col-xs-3 col-sm-3">
-                    <div className="card text-white">
-                        <img src={image1} className="card-img" alt="..."/>
-                        <div className="card-img-overlay" style={{paddingTop:"5rem!important",textAlign:"center"}}>
-                            <h5 className="card-title">Qui Som</h5>
-                            {/* <p className="card-text">This is a wider card</p> */}
-                        </div>
-                    </div>
+                <div className="col-lg-4">
+                {/* <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
+                <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
+                    <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
+                <div className='color-div-card'>
+                    <h2>L'AFA</h2>
+                    <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
                 </div>
-                <div className="col-xs-3 col-sm-3">
-                    <div className="card text-white">
-                        <img src={image1} className="card-img" alt="..."/>
-                        <div className="card-img-overlay" style={{paddingTop:"5rem!important",textAlign:"center"}}>
-                            <h5 className="card-title">Noticies</h5>
-                            {/* <p className="card-text">This is a wider card</p> */}
-                        </div>
-                    </div>    
                 </div>
-                <div className="col-xs-3 col-sm-3">
-                    <div className="card text-white">
-                        <img src={image1} className="card-img" alt="..."/>
-                        <div className="card-img-overlay" style={{paddingTop:"5rem!important",textAlign:"center"}}>
-                            <h5 className="card-title">Extraescolars</h5>
-                            {/* <p className="card-text">This is a wider card</p> */}
-                        </div>
-                    </div>    
+                <div className="col-lg-4">
+                {/* <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
+                <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
+                    <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
+                <div className='color-div-card'>
+                    <h2>SERVEIS</h2>
+                    <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
                 </div>
-                <div className="col-xs-3 col-sm-3">
-                    <div className="card text-white">
-                        <img src={image1} className="card-img" alt="..."/>
-                        <div className="card-img-overlay" style={{paddingTop:"5rem!important",textAlign:"center"}}>
-                            <h5 className="card-title">Serveis</h5>
-                            {/* <p className="card-text">This is a wider card</p> */}
-                        </div>
-                    </div>
+                </div>
+                <div className="col-lg-4">
+                {/* <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
+                <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
+                    <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
+                <div className='color-div-card'>
+                    <h2>ESPAI DE TRANSPARENCIA</h2>
+                    <p>And lastly this, the third column of representative placeholder content.</p>
+                </div>
                 </div>
             </div>
             <div className="row">
-                <div className="col-xs-4 col-sm-4">
-                    <div className="card text-white">
-                        <img src={image1} className="card-img img-1-card-row-2" alt="..."/>
-                        <div className="card-img-overlay card-img-overlay-img-1-card-row-2" style={{paddingTop:"5rem!important",textAlign:"center"}}>
-                            <h5 className="card-title">Documents</h5>
-                            {/* <p className="card-text">This is a wider card</p> */}
-                        </div>
-                    </div>
+                <div className="col-lg-4 margin-left-col-lg-4">
+                {/* <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
+                <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
+                    <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
+                <div className='color-div-card'>
+                    <h2>NOTICIES</h2>
+                    <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
                 </div>
-                <div className="col-xs-4 col-sm-4">
-                    <div className="card text-white">
-                        <img src={image1} className="card-img img-2-card-row-2" alt="..."/>
-                        <div className="card-img-overlay card-img-overlay-img-2-card-row-2" style={{paddingTop:"5rem!important",textAlign:"center"}}>
-                            <h5 className="card-title">Families</h5>
-                            {/* <p className="card-text">This is a wider card</p> */}
-                        </div>
-                    </div>    
                 </div>
-                <div className="col-xs-4 col-sm-4">
-                    <div className="card text-white">
-                        <img src={image1} className="card-img img-3-card-row-2" alt="..."/>
-                        <div className="card-img-overlay card-img-overlay-img-3-card-row-2" style={{paddingTop:"5rem!important",textAlign:"center"}}>
-                            <h5 className="card-title">Casal d'estiu</h5>
-                            {/* <p className="card-text">This is a wider card</p> */}
-                        </div>
-                    </div>    
+                <div className="col-lg-4">
+                {/* <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
+                <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
+                    <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
+                <div className='color-div-card'>
+                    <h2>ESDEVENIMENTS</h2>
+                    <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
                 </div>
-            </div>  
+                </div>
+                <div className="col-lg-4 d-none">
+                {/* <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
+                <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
+                    <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
+                <h2>Heading</h2>
+                <p>And lastly this, the third column of representative placeholder content.</p>
+                </div>
+            </div>
         </>
-        
-        // <div className="card-group">
-        //     <div class="card" style={{width:"18rem"}}>
-        //         <img src={image1} class="card-img-top" alt="..."/>
-        //         <div class="card-body">
-        //             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        //         </div>
-        //     </div>
-        //     <div class="card" style={{width:"18rem"}}>
-        //         <img src={image1} class="card-img-top" alt="..."/>
-        //         <div class="card-body">
-        //             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        //         </div>
-        //     </div>
-        //     <div class="card" style={{width:"18rem"}}>
-        //         <img src={image1} class="card-img-top" alt="..."/>
-        //         <div class="card-body">
-        //             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        //         </div>
-        //     </div>
-        //     <div class="card" style={{width:"18rem"}}>
-        //         <img src={image1} class="card-img-top" alt="..."/>
-        //         <div class="card-body">
-        //             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        //         </div>
-        //     </div>
-        //     <div className="card bg-dark text-white">
-        //         <img src={image1} className="card-img" alt="..."/>
-        //         <div className="card-img-overlay">
-        //             <h5 className="card-title">Card title</h5>
-        //             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        //             <p className="card-text">Last updated 3 mins ago</p>
-        //         </div>
-        //     </div>
-        //     <div className="card bg-dark text-white">
-        //         <img src={image1} className="card-img" alt="..."/>
-        //         <div className="card-img-overlay">
-        //             <h5 className="card-title">Card title</h5>
-        //             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        //             <p className="card-text">Last updated 3 mins ago</p>
-        //         </div>
-        //     </div>
-        // </div>
     );
 }
 
