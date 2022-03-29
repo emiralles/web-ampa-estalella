45f3ff194a973c94c0287fa5e0d6f167ec4b4409
emiralles <emiralles_yataco@hotmail.com>
2022-03-30 01:04:46 +0200

    Add files via upload

---
+++ src/components/serveis/Extraescolars.js
@@ -0,0 +1,122 @@
+import imagen1 from "./images/LogoRobotica.PNG";
+
+function Extraescolars() {
+    return (  
+        <>
+            <hr className="featurette-divider"></hr>
+            <div className="containerH1"><h1>Extraescolars</h1></div>
+            <p className="fs-5 col-md-12">L’AMPA de l’Escola Estalella i Graells ofereix activitats extraescolars de qualitat, atractives i que complementen l’ensenyament lectiu. Esperem que compleixin amb les vostres preferències i interessos i que les gaudiu molt!</p>
+            <hr className="col-3 col-md-12 mb-5"></hr>
+            <div className="row g-5">
+                <div className="col-md-12">
+                    <h2>Objectius</h2>
+                    <hr className="col-3 col-md-2 mb-5"></hr>
+                </div>
+            </div>
+            <div className="album py-5 bg-light">
+                <div className="container">
+
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                    <div className="col">
+                    <div className="card shadow-sm">
+                        <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                        <div className="card-body">
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="col">
+                    <div className="card shadow-sm">
+                        <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                        <div className="card-body">
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="col">
+                    <div className="card shadow-sm">
+                        <svg claclassNamess="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                        <div className="card-body">
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className="col">
+                    <div className="card shadow-sm">
+                        <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                        <div className="card-body">
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="col">
+                    <div className="card shadow-sm">
+                        <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                        <div className="card-body">
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="col">
+                    <div className="card shadow-sm">
+                        <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                        <div className="card-body">
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className="col">
+                    <div className="card shadow-sm">
+                        <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                        <div className="card-body">
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="col">
+                    <div className="card shadow-sm">
+                        <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                        <div className="card-body">
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="col">
+                    <div className="card shadow-sm">
+                        <div className="container container-background">
+                            <div className="row row-cols-2">
+                                <div className=" col-sm-8 col-md-8 title-row-principal">Robotica</div>
+                                <div className=" col-sm-4 col-md-4"><img className="imagen-extraescolar" src={imagen1} alt="" /></div>
+                                {/* <svg className="bd-placeholder-img card-img-top" width="100%" height="35%" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#55595c"></rect></svg> */}
+                            </div>
+                            <div className="row row-cols-3 col-row-subtitle">
+                                <div className=" col-sm-4 col-md-4">1r a 6e</div>
+                                <div className=" col-sm-4 col-md-4">280</div>
+                                <div className=" col-sm-4 col-md-4">1 dia setmanal</div>
+                            </div>
+                        </div>
+                        <div className="card-body">
+                            <p className="card-text">Introducció a la robòtica, investigació del funcionament de màquines, mecanismes i programació utilitzant els primers sensors.</p>
+                            <p className="card-text">Programació amb un coneixement previ adquirit, dissenyant, construint i programant el propi robot o resolent diferents missions en un tauler de joc.</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <hr className="featurette-divider"></hr>
+        </>
+    );
+}
+
+export default Extraescolars;
