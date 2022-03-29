b1c7afccf991f26984f761347bb95518e5cf90db
emiralles <emiralles_yataco@hotmail.com>
2022-03-29 22:18:22 +0200

    Add files via upload

--- src/components/serveis/Menjador.js
+++ src/components/serveis/Menjador.js
@@ -3,7 +3,7 @@ function Menjador() {
         <>
             <hr className="featurette-divider"></hr>
             <div className="containerH1"><h1>Menjador</h1></div>
-            <hr className="featurette-divider"></hr>
+            {/* <hr className="featurette-divider"></hr> */}
             <div className="video-responsive">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/Wox8BHyJ0XE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
                 </iframe>
