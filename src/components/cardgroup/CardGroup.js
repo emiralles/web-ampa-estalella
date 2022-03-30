761d2dbd2ed5b6145674217e9b8ff69ec6edec84
emiralles <emiralles_yataco@hotmail.com>
2022-03-31 00:20:12 +0200

    Add files via upload

--- src/components/cardgroup/CardGroup.js
+++ src/components/cardgroup/CardGroup.js
@@ -6,58 +6,56 @@ function CardGroup() {
             <hr className="featurette-divider"></hr>
             <div className="row">
                 <div className="col-lg-4">
-                {/* <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
-                <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
-                    <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
-                <div className='color-div-card'>
-                    <h2>L'AFA</h2>
-                    {/* <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p> */}
-                </div>
-                </div>
-                <div className="col-lg-4">
-                {/* <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
-                <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
-                    <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
-                <div className='color-div-card'>
-                    <h2>SERVEIS</h2>
-                    {/* <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p> */}
-                </div>
+                    <a href="/">
+                        <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
+                            <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
+                        <div className='color-div-card'>
+                            <h2 className="title-a-icon">L'AFA</h2>
+                        </div>
+                    </a>
                 </div>
                 <div className="col-lg-4">
-                {/* <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
-                <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
-                    <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
-                <div className='color-div-card'>
-                    <h2>ESPAI DE TRANSPARENCIA</h2>
-                    {/* <p>And lastly this, the third column of representative placeholder content.</p> */}
+                    <a href="/">
+                        <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
+                        <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
+                        <div className='color-div-card'>
+                            <h2 className="title-a-icon">SERVEIS</h2>
+                        </div>        
+                    </a>
                 </div>
+                <div className="col-lg-4">
+                    <a href="/">
+                        <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
+                        <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
+                        <div className='color-div-card'>
+                            <h2 className="title-a-icon">ESPAI DE TRANSPARENCIA</h2>
+                        </div>        
+                    </a>
                 </div>
             </div>
             <div className="row">
                 <div className="col-lg-4 margin-left-col-lg-4">
-                {/* <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
-                <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
-                    <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
-                <div className='color-div-card'>
-                    <h2>NOTICIES</h2>
-                    {/* <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p> */}
-                </div>
+                    <a href="/">
+                        <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
+                        <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
+                        <div className='color-div-card'>
+                            <h2 className="title-a-icon">NOTICIES</h2>
+                        </div>
+                    </a>
                 </div>
                 <div className="col-lg-4">
-                {/* <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
-                <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
-                    <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
-                <div className='color-div-card'>
-                    <h2>ESDEVENIMENTS</h2>
-                    {/* <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p> */}
-                </div>
+                    <a href="/">
+                        <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
+                            <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
+                        <div className='color-div-card'>
+                            <h2 className="title-a-icon">ESDEVENIMENTS</h2>
+                        </div>    
+                    </a>
                 </div>
                 <div className="col-lg-4 d-none">
-                {/* <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
-                <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
-                    <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
-                <h2>Heading</h2>
-                {/* <p>And lastly this, the third column of representative placeholder content.</p> */}
+                    <img src={imag1} className="bd-placeholder-img rounded-circle" width="140" height="140" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"/>
+                        <title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect>
+                    <h2>Heading</h2>
                 </div>
             </div>
             <hr className="featurette-divider"></hr>
