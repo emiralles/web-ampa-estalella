774314a6109ad3844813fd6f5785eb30ffb36c8e
emiralles <emiralles_yataco@hotmail.com>
2022-05-26 21:44:54 +0200

    Add files via upload

--- src/models/extraescolars.js
+++ src/models/extraescolars.js
@@ -1,7 +1,8 @@
 export class extraEscolars{
-    constructor(Uid,path,title,parragraph,dateStart,dateEnd,mainText,namePhoto,urlPhoto,whenDo,howTimes,price,grupsToDo){
+    constructor(Uid,path,plazas,title,parragraph,dateStart,dateEnd,mainText,namePhoto,urlPhoto,whenDo,howTimes,price,grupsToDo){
         this.Uid = Uid;
         this.path = path;
+        this.plazas = plazas;
         this.title= title;
         this.parragraph= parragraph;
         this.dateStart= dateStart;
