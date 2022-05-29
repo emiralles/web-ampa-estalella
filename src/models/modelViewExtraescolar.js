bc159fd724897319a224c4a9dede90af877bb860
emiralles <emiralles_yataco@hotmail.com>
2022-05-29 13:19:30 +0200

    Add files via upload

---
+++ src/models/modelViewExtraescolar.js
@@ -0,0 +1,12 @@
+export class modelViewExtraescolar{
+    constructor(Uid,title,dateStart,dateEnd,whenDo,howTimes,price,grupsToDo){
+        this.Uid = Uid;
+        this.title= title;
+        this.dateStart= dateStart;
+        this.dateEnd= dateEnd;
+        this.whenDo= whenDo;
+        this.howTimes= howTimes;
+        this.price= price;
+        this.grupsToDo = grupsToDo;
+    }
+}
