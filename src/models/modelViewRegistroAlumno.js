a04c2add3f51eb42de697ee84652efa912560e1b
emiralles <emiralles_yataco@hotmail.com>
2022-05-31 20:25:17 +0200

    Add files via upload

---
+++ src/models/modelViewRegistroAlumno.js
@@ -0,0 +1,14 @@
+export class modelViewRegistroAlumno{
+    constructor(uid,titleExtraEscolar,nombreTutor,apellidosTutor,email,nombreAlumno,apellidosAlumno,edad,curso,grupos){
+        this.uid = uid;
+        this.titleExtraEscolar= titleExtraEscolar;
+        this.nombreTutor= nombreTutor;
+        this.apellidosTutor = apellidosTutor;
+        this.email= email;
+        this.nombreAlumno= nombreAlumno;
+        this.apellidosAlumno = apellidosAlumno;
+        this.edad = edad;
+        this.curso= curso;
+        this.grupos = grupos;
+    }
+}
