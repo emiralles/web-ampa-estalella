35e5d2c61fce540fb6e2e15524263a51919b4931
Enrique Miralles Yataco <emiralles_yataco@hotmail.com>
2022-11-28 17:26:46 +0100

    commit 2811-1

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -7,7 +7,64 @@ import { getAllCollections, getUrlImage } from "../../db/crudDB";
 import { itemcarousel } from "../../models/itemCarousel";
 
 
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+//import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+//import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+//import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+// const images = [
+//   {
+//     label: 'San Francisco – Oakland Bay Bridge, United States',
+//     imgPath:
+//       'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+//   },
+//   {
+//     label: 'Bird',
+//     imgPath:
+//       'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+//   },
+//   {
+//     label: 'Bali, Indonesia',
+//     imgPath:
+//       'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+//   },
+//   {
+//     label: 'Goč, Serbia',
+//     imgPath:
+//       'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+//   },
+// ];
+
+
 function Carousel() {
+
+    const theme = useTheme();
+    const [activeStep, setActiveStep] = React.useState(0);
+    //const [maxSteps, setmaxSteps] = React.useState(1);
+    //let maxSteps = 0; //images.length;
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStepChange = (step) => {
+        setActiveStep(step);
+    };
+
 
     const [listEsdeveniments,setListEsdeveniments] = useState([]);
 
@@ -22,61 +79,121 @@ function Carousel() {
               imgUrl.then((rstUrl)=>{
                 let item = new itemcarousel(doc.id,doc.path,"",doc.title,doc.dateCreation,doc.namePhoto,rstUrl); 
                 setListEsdeveniments(arr => [...arr, item]);
-              });
+                
+            });
             })
           })
-          
         }
         
         handleLoad();
+        //setmaxSteps(listEsdeveniments.length);
+        //maxSteps = listEsdeveniments.length;
     
     },[]);
 
     if(listEsdeveniments != null && listEsdeveniments.length > 0)
     {
         return(
-            <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-indicators">
-                    {
-                        listEsdeveniments.map((esdevenim,index) => {
-                            return (
-                                <button type="button" key={index} data-bs-target="#myCarousel" data-bs-slide-to={index} className={index === 0 ?"active":""} aria-label={`Slide ${index}`}></button>
-                            )
-                        })
-                    }
-                    {/* // <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="" aria-label="Slide 1"></button>
-                    // <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" className="active" aria-current="true"></button>
-                    // <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" className=""></button> */}
-                </div>
-                <div className="carousel-inner">
-                
-                    {
-                        listEsdeveniments.map((esdevenim,index) => {
-                            return (
-                                <div key={index} className={index === 0 ? "carousel-item active":"carousel-item"}>
-                                    <img className="bd-placeholder-img" width="100%" height="100%" src={esdevenim.urlPhoto} aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false" alt=""/>
-                                </div>
-                            )
-                        })
-                    }
-                    
-                    {/* <div className="carousel-item ">
-                        <img className="bd-placeholder-img" width="100%" height="100%" src={imag2} aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false" alt=""/>
+            <Box sx={{ maxWidth: '100%', flexGrow: 1 }}>
+                <Paper
+                    square
+                    elevation={0}
+                    sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    height: 80,
+                    pl: 1,
+                    bgcolor: 'background.default',
+                    }}
+                >
+                    {/* <Typography>{images[activeStep].label}</Typography> */}
+                </Paper>
+                <AutoPlaySwipeableViews
+                    axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                    index={activeStep}
+                    onChangeIndex={handleStepChange}
+                    enableMouseEvents
+                >
+                    {listEsdeveniments.map((step, index) => (
+                    <div key={index}>
+                        {Math.abs(activeStep - index) <= 2 ? (
+                        <Box
+                            component="img"
+                            sx={{
+                            height: 255,
+                            display: 'block',
+                            maxWidth: '100%',
+                            overflow: 'hidden',
+                            width: '100%',
+                            }}
+                            src={step.urlPhoto}
+                            alt={index}
+                        />
+                        ) : null}
                     </div>
-                    <div className="carousel-item">
-                        <img className="bd-placeholder-img" width="100%" height="100%" src={imag3} aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false" alt=""/>
-                    </div> */}
+                    ))}
+                </AutoPlaySwipeableViews>
+                <MobileStepper
+                    steps={listEsdeveniments.length}
+                    position="static"
+                    activeStep={activeStep}
+                    nextButton={
+                    <Button
+                        size="small"
+                        onClick={handleNext}
+                        disabled={activeStep === listEsdeveniments.length - 1}
+                    >
+                        {/* Next
+                        {theme.direction === 'rtl' ? (
+                        <KeyboardArrowLeft />
+                        ) : (
+                        <KeyboardArrowRight />
+                        )} */}
+                    </Button>
+                    }
+                    backButton={
+                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                        {/* {theme.direction === 'rtl' ? (
+                        <KeyboardArrowRight />
+                        ) : (
+                        <KeyboardArrowLeft />
+                        )}
+                        Back */}
+                    </Button>
+                    }
+                />
+            </Box>
+            // <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
+            //     <div className="carousel-indicators">
+            //         {
+            //             listEsdeveniments.map((esdevenim,index) => {
+            //                 return (
+            //                     <button type="button" key={index} data-bs-target="#myCarousel" data-bs-slide-to={index} className={index === 0 ?"active":""} aria-label={`Slide ${index}`}></button>
+            //                 )
+            //             })
+            //         }
+            //     </div>
+            //     <div className="carousel-inner">
                 
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
-            </div>
+            //         {
+            //             listEsdeveniments.map((esdevenim,index) => {
+            //                 return (
+            //                     <div key={index} className={index === 0 ? "carousel-item active":"carousel-item"}>
+            //                         <img className="bd-placeholder-img" width="100%" height="100%" src={esdevenim.urlPhoto} aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false" alt=""/>
+            //                     </div>
+            //                 )
+            //             })
+            //         }
+            //     </div>
+            //     <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+            //         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            //         <span className="visually-hidden">Previous</span>
+            //     </button>
+            //     <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+            //         <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            //         <span className="visually-hidden">Next</span>
+            //     </button>
+            // </div>
         );
         
     }
